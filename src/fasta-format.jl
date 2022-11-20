9537d39815e270ed6ebc341041a52282d5879c7c
Marco Matthies <marco.matthies@gmail.com>
2022-11-20 20:27:21 +0100

    fasta: add functions that write to an IO object

--- src/fasta-format.jl
+++ src/fasta-format.jl
@@ -7,9 +7,13 @@
 
 function _write_fasta(path::AbstractString, title_seq_iterable)
     open(path, "w") do io
-        for (title, seq) in title_seq_iterable
-            println(io, ">", title, "\n", seq)
-        end
+        _write_fasta(io, title_seq_iterable)
+    end
+end
+
+function _write_fasta(io::IO, title_seq_iterable)
+    for (title, seq) in title_seq_iterable
+        println(io, ">", title, "\n", seq)
     end
 end
 
@@ -18,3 +22,9 @@ _write_fasta(path::AbstractString, seqs::Vector{<:AbstractString}) =
 
 _write_fasta(path::AbstractString, seq::AbstractString) =
     _write_fasta(path, [("", seq)])
+
+_write_fasta(io::IO, seqs::Vector{<:AbstractString}) =
+    _write_fasta(io, [("", s) for s in seqs])
+
+_write_fasta(io::IO, seq::AbstractString) =
+    _write_fasta(io, [("", seq)])
